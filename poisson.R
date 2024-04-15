c8a4eb4f9ca360b05bd587973fcd2e486581f86d
yusufburak-2004 <145719589+yusufburak-2004@users.noreply.github.com>
2024-04-15 16:01:35 +0300

    Update poisson.R

--- poisson.R
+++ poisson.R
@@ -1,1 +1,11 @@
-a
+library(tidyverse)
+# FACTORY PRODUCTS 1 IN EVERY 2 MINUTE
+pos <- rpois(1000,2)
+  data <- data.frame(pos)
+ggplot(data,aes(pos))+
+  geom_histogram()+
+  labs(x='MINUTES')
+ppois(4,2) - dpois(4,2)
+##EQUAL WITH BELOW IF SAMPLE INCREASE
+# pexp(target,1/mean)
+pexp(4,1/2)
